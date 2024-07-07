b0b79ee195425bd89664b3ea246b6890f3a47374
rafinhadufluxo <arruda.rafa.ra@gmail.com>
2024-07-07 08:56:22 -0400

    ajuste de url de pages

--- src/App.js
+++ src/App.js
@@ -1,60 +1,52 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import ProfileCard from './Components/ProfileCard';
-import AppRoutes from './Routes'; // Ajuste o nome do arquivo
+import AppRoutes from './Routes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSun, faMoon, faLanguage } from '@fortawesome/free-solid-svg-icons'; // Importe os ícones necessários
+import { faSun, faMoon, faLanguage } from '@fortawesome/free-solid-svg-icons';
 import './assets/styles.css';
 
 function App() {
   const [isDay, setIsDay] = useState(true);
-  const [language, setLanguage] = useState('en'); // Estado para controlar o idioma ('en' para inglês por padrão)
+  const [language, setLanguage] = useState('en');
 
   const toggleBackgroundColor = () => {
     setIsDay(!isDay);
   };
 
   const toggleLanguage = () => {
-    // Lógica para alternar entre os idiomas
     switch (language) {
       case 'en':
-        setLanguage('pt'); // Alterna para português
+        setLanguage('pt');
         break;
       case 'pt':
-        setLanguage('es'); // Alterna para espanhol
+        setLanguage('es');
         break;
       case 'es':
-        setLanguage('en'); // Alterna de volta para inglês
+        setLanguage('en');
         break;
       default:
-        setLanguage('en'); // Caso padrão, configura para inglês
+        setLanguage('en');
         break;
     }
   };
 
   return (
-
     <div className="app-container" style={{ backgroundColor: 'rgb(17, 24, 39)', background: isDay ? 'linear-gradient(45deg, var(--purple), var(--purple-light))' : 'linear-gradient(45deg, var(--classic), var(--classic))' }}>
-
       <div className="button-container">
         <button className="toggle-theme-btn" onClick={toggleBackgroundColor}>
           <FontAwesomeIcon icon={isDay ? faMoon : faSun} />
         </button>
-
         <button className="toggle-language-btn" onClick={toggleLanguage}>
           <FontAwesomeIcon icon={faLanguage} />
         </button>
       </div>
-
-      <Router basename='/'>
+      <Router>
         <div className="container">
           <ProfileCard />
-
           <div className="page-content">
-            {/* Passa a variável de idioma para o componente AppRoutes */}
             <AppRoutes language={language} />
           </div>
-
         </div>
       </Router>
     </div>
